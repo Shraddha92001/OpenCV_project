9371354968b9b6330d4fa6b9b301f352ce25c698
shraddha-singh <shraddhasingh92001@gmail.com>
2023-06-24 22:08:26 +0530

    arrow_line

--- geometric_shapes.py
+++ geometric_shapes.py
@@ -1,6 +1,7 @@
 import cv2
-img = cv2.imread('photos/lena.jpg',0)
-img = cv2.line(img ,(0,0),(255,255),(255,0,0))
+img = cv2.imread('photos/lena.jpg',1)
+img = cv2.line(img ,(0,0),(255,255),(255,96,44),5)
+img = cv2.arrowedLine(img ,(0,255),(255,255),(255,0,0))
 
 cv2.imshow('image',img)
 cv2.waitKey(0)
